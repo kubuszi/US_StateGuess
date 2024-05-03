54f9c191a1ab78b8a8c5b90af16fd563c1d71c21
kubuszi <kubushii2@wp.pl>
2024-05-03 12:20:42 +0200

    Read states names from csv and compare to user answer

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("US State Guess")
 image = "blank_states_img.gif"
 screen.addshape(image)
-
 turtle.shape(image)
 
-answer = screen.textinput(title="Guess the State", prompt="What's another state's name?")
-def get_mouse_cor(x, y):
-    print(x, y)
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
 
+while len(guessed_states) < 50:
+    answer = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?").title()
 
-turtle.onscreenclick(get_mouse_cor)
+    if answer in all_states:
+        guessed_states.append(answer)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer)
 
-turtle.mainloop()
+screen.exitonclick()
